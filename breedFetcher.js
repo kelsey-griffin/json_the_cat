bc2cff6a06e5b1025ef66937e65d5d9171afd4b0
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-12 17:14:13 +0000

    refactored breedFetcher and added mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,19 @@
-const arg = process.argv[2];
 const request = require('request');
-request(`https://api.thecatapi.com/v1/images/search?breed_ids=${arg.substring(0,4)}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('No such breed exists!');
-    process.exit();
-  }
-  console.log(data[0]);
-});
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName.substring(0,4)}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, 'No such breed exists!');
+    } else {
+      callback(null, data[0].breeds[0].description.trim());
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
